ea26ff01c1dc01ebb3fe3accf90ed62b3b55a8de
Ilya Samarin <ilia-samarin@bk.ru>
2023-12-10 18:12:19 +0700

    start work for changelevel for admins

--- Source/AdminCommands.cs
+++ Source/AdminCommands.cs
@@ -15,6 +15,7 @@ using PRTelegramBot.Models.CallbackCommands;
 using PRTelegramBot.Models.InlineButtons;
 using PRTelegramBot.Helpers.TG;
 using PRTelegramBot.Helpers;
+using Cs2Telegram.Models;
 
 namespace Cs2Telegram
 {
@@ -24,6 +25,58 @@ namespace Cs2Telegram
         {
             string msg = "Access denied";
             Helper.SendMessage(botClient, update.GetChatId(), msg);
+        }
+
+        [ReplyMenuHandler("MapList")]
+        public static async Task MapList(ITelegramBotClient botClient, Update update)
+        {
+            if (!botClient.IsAdmin(update.GetChatId()))
+            {
+                await AccessDenied(botClient, update);
+                return;
+            }
+
+
+            Server.NextFrame(() =>
+            {
+                try
+                {
+                    Console.WriteLine(NativeAPI.GetGameDirectory());
+                    var maps = Server.GetMapList();
+                    GenerateMapList(botClient, update, maps);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex);
+                    Helper.SendMessage(botClient, update.GetChatId(), $"Error when getting a list of maps: {ex}");
+                }
+            });
+        }
+
+        public static async Task GenerateMapList(ITelegramBotClient botClient, Update update, string[] maps)
+        {
+            string message = "";
+            var options = new OptionMessage();
+
+            if (maps.Length > 0)
+            {
+                message += "Server maps:\n\n";
+                List<IInlineContent> buttons = new();
+                foreach (var map in maps)
+                {
+                    var menuItem = new InlineCallback<MapTCommand>(map, HeadersCommads.SelectMap, new MapTCommand(map));
+                    buttons.Add(menuItem);
+
+                }
+                var menu = MenuGenerator.InlineKeyboard(1, buttons);
+                options.MenuInlineKeyboardMarkup = menu;
+            }
+            else
+            {
+                message = "No maps :(";
+            }
+
+            Helper.SendMessage(botClient, update.GetChatId(), message, options);
         }
     }
 }
---
+++ Source/Models/MapTCommand.cs
@@ -0,0 +1,21 @@
+﻿using Newtonsoft.Json;
+using PRTelegramBot.Models.CallbackCommands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cs2Telegram.Models
+{
+    public class MapTCommand : TCommandBase
+    {
+        [JsonProperty("1")]
+        public string MapName { get; set; }
+
+        public MapTCommand(string mapName)
+        {
+            MapName = mapName;
+        }
+    }
+}
