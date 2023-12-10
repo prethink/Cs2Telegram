b3a1993a60cebfd86618bdea296ec091a706175a
Ilya Samarin <ilia-samarin@bk.ru>
2023-12-10 15:43:30 +0700

    source file in folder source

---
+++ Source/AdminCommands.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Telegram.Bot.Types;
+using Telegram.Bot;
+using PRTelegramBot.Extensions;
+
+namespace Cs2Telegram
+{
+    public static class AdminCommands
+    {
+        public static async Task AccessDenied(ITelegramBotClient botClient, Update update)
+        {
+            string msg = "Access denied";
+            await Helper.SendMessage(botClient, update.GetChatId(), msg);
+        }
+    }
+}
