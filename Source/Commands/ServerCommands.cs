e6ad74b3fcf146bd7d1f37bfe3a440ff95aa2341
Ilya Samarin <ilia-samarin@bk.ru>
2023-12-11 19:37:30 +0700

    add command from txt file

--- Source/Commands/ServerCommands.cs
+++ Source/Commands/ServerCommands.cs
@@ -8,8 +8,14 @@ using Telegram.Bot;
 using PRTelegramBot.Attributes;
 using PRTelegramBot.Models;
 using PRTelegramBot.Extensions;
-using static System.Runtime.InteropServices.JavaScript.JSType;
+using File = System.IO.File;
 using CounterStrikeSharp.API;
+using Cs2Telegram.Models;
+using CounterStrikeSharp.API.Core;
+using PRTelegramBot.Models.InlineButtons;
+using PRTelegramBot.Models.CallbackCommands;
+using PRTelegramBot.Models.Interface;
+using PRTelegramBot.Helpers.TG;
 
 namespace Cs2Telegram.Commands
 {
@@ -28,10 +34,37 @@ namespace Cs2Telegram.Commands
                 return;
             }
 
+            var pathCommands = Path.Combine(TelegramCore.PluginPath, TelegramCore.FILE_SERVER_COMMANDS);
+
+            if(!File.Exists(pathCommands))
+            {
+                File.Create(pathCommands);
+            }
+
+            var lines = File.ReadAllLines(pathCommands);
+            var inlineMenuItems = new List<IInlineContent>();
+            if (lines.Length > 0)
+            {
+                foreach (var command in lines.Where(command => !string.IsNullOrWhiteSpace(command)))
+                {
+                    var commandItem = new InlineCallback<ServerExecuteTCommand>(command, HeadersCommads.ExecuteServerCommand, new ServerExecuteTCommand(command));
+                    inlineMenuItems.Add(commandItem);
+                }
+            }
+
+            var options = new OptionMessage();
+            if(inlineMenuItems.Count > 0)
+            {
+                options.MenuInlineKeyboardMarkup = MenuGenerator.InlineKeyboard(MenuGenerator.InlineButtons(1, inlineMenuItems));
+            }
+            else
+            {
+                options.MenuReplyKeyboardMarkup = botClient.GenerateOnlyMenu(update.GetChatId());
+            }
+
+
             string msg = SERVER_COMMAND_MSG;
             update.RegisterNextStep(new StepTelegram(ExecuteCommandInServerHandler, null));
-            var options = new OptionMessage();
-            options.MenuReplyKeyboardMarkup = botClient.GenerateOnlyMenu(update.GetChatId());
             Helper.SendMessage(botClient, update, msg, options);
         }
 
@@ -46,13 +79,40 @@ namespace Cs2Telegram.Commands
                 {
                     Server.ExecuteCommand(msg);
                 });
-                Helper.SendMessage(botClient, update, $"🗯️ Server try execute command: {msg}\n\n{SERVER_COMMAND_MSG}");
+                Helper.SendMessage(botClient, update, $"🗯️ Server try execute command: {msg}\n\n{SERVER_BACK_TO_MENU}");
             }
             else
             {
                 Helper.SendMessage(botClient, update, $"Error empty message, try again");
             }
+        }
 
+        [InlineCallbackHandler<HeadersCommads>(HeadersCommads.ExecuteServerCommand)]
+        public static async Task ExecuteCommandInServerHandler(ITelegramBotClient botClient, Update update)
+        {
+            if (!botClient.IsAdmin(update.GetChatId()))
+            {
+                await AdminCommands.AccessDenied(botClient, update);
+                return;
+            }
+
+            var command = InlineCallback<ServerExecuteTCommand>.GetCommandByCallbackOrNull(update.CallbackQuery.Data);
+            if (command != null)
+            {
+                if (!string.IsNullOrWhiteSpace(command.Data.Command))
+                {
+                    string serverCommand = command.Data.Command;
+                    Server.NextFrame(() =>
+                    {
+                        Server.ExecuteCommand(serverCommand);
+                    });
+                    Helper.SendMessage(botClient, update, $"🗯️ Server try execute command: {serverCommand}\n\n{SERVER_COMMAND_MSG}");
+                }
+                else
+                {
+                    Helper.SendMessage(botClient, update, $"Error empty message, try again");
+                }
+            }
         }
 
         [ReplyMenuHandler(Constants.SERVER_SEND_MESSAGE_BUTTON)]
